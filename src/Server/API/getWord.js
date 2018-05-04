37e95fc8a798261c398ffdd82e60cc26ab9748be
WendyMin <min711s9d31@126.com>
2018-05-04 17:41:27 +0800

    +msg

--- src/Server/API/getWord.js
+++ src/Server/API/getWord.js
@@ -4,7 +4,7 @@ const pythonFilename = "query_word.py";
 
 module.exports = ( req, res ) => {
   var { query_words } = req.body;
-  query_words = query_words || "";
+  // query_words = query_words || "";
   const [ errCode, result ] = callPython(
     pythonFilename,
     `${query_words}`
